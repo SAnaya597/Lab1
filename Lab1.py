0fa1d34b8a9f0290e7776ae7ce4a78c9dcb19079
Spenser Anaya <Spenser_Anaya@yahoo.com>
2025-08-26 17:40:43 -0700

    Initial commit

---
+++ Lab1.py
@@ -0,0 +1,34 @@
+import math
+radius = 5
+area = math.pi * radius ** 2
+print("area of the Circle:", area )
+
+import math
+radius = 3
+volume = (4/3) * math.pi * radius ** 3
+print("volume of the Circle:", volume)
+
+import math
+a = 3
+b = 4
+c = math.sqrt(a**2 + b**2)
+print("hypotenuse:", c)
+
+first_name = "Spenser"
+last_name = "Anaya"
+full_name = first_name + " " + last_name
+print(full_name)
+print("Length of full name:", len(full_name))
+print("Uppercase:", full_name.upper())
+print("Lowercase:", full_name.lower())
+
+age = 34
+height_feet = 6
+weight_pounds = 270
+print("Age:", age)
+print("Height:", height_feet)
+print("Weight:", weight_pounds)
+height_inches = height_feet * 6
+bmi = (weight_pounds / (height_inches) ** 2) * 703
+print("BMI:", bmi)
+
