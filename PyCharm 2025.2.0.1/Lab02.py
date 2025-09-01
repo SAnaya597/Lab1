db306095af5150bb39f9529e1bd3cbfed23eb199
Spenser Anaya <Spenser_Anaya@yahoo.com>
2025-09-01 10:29:36 -0700

    Intial commit

---
+++ PyCharm 2025.2.0.1/Lab02.py
@@ -0,0 +1,73 @@
+print(f"PART 1")
+name = "Spenser Anaya"
+age = 34
+height = 6
+favorite_color = "Black"
+print(name)
+print(age)
+print(height)
+print(favorite_color)
+print(name, age, height, favorite_color)
+print(f"Hi:I'm {name}, I am {age} years old, I am {height} ft tall, and my favorite color is {favorite_color}.")
+print(f"""
+name: {name}
+age: {age}
+height: {height}
+favorite color: {favorite_color}""")
+
+
+print(f"PART 2")
+import math
+radius = 5
+circle_area = math.pi * radius ** 2
+
+print(f"circle area = {circle_area:.1f}")
+import math
+age = 34
+height = 6
+sqrt_age = math.sqrt(age)
+print(f"square root of age = {sqrt_age:.1f}")
+
+sine_height = math.sin(height)
+cosine_height = math.cos(height)
+print(f"sine = {sine_height:.1f}")
+print(f"cos = {cosine_height:.1f}")
+
+
+print(f"PART 3")
+
+import math
+age = 34
+height = 6
+sum = age + 5
+difference = height - 4
+product = age * height
+quotient = height / 2
+remainder = age % 3
+power = age ** 2
+print(f"""sum = {sum:.1f}
+difference = {difference:.1f}
+product = {product:.1f}
+quotient = {quotient:.1f}
+remainder = {remainder:.1f}
+power = {power:.1f}""")
+print(f"PART3")
+
+
+
+fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+celsius = (fahrenheit - 32) * 5 / 9
+
+
+print(f"{fahrenheit}°F is equal to {celsius:.1f}°C")
+
+
+
+
+
+
+
+
+
+
+
